00a17878ed278f9d349f60fab9b33b92a3f92c26
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2023-09-19 00:47:53 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,62 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.getElementById("excuse");
+  const btn = document.getElementById("btn");
+
+  const who = [
+    "My dog",
+    "My goat",
+    "My fish",
+    "My bird",
+    "My neighbor",
+    "My grandpa",
+    "My granny",
+    "My toaster",
+    "A unicorn",
+    "A UFO",
+    "Spiderman"
+  ];
+
+  const what = [
+    "ate",
+    "burned",
+    "disappeared",
+    "shredded",
+    "teleported",
+    "sold",
+    "rented",
+    "made toast with",
+    "hid"
+  ];
+
+  const whatAvoid = [
+    "my homework",
+    "my presentation",
+    "my uniform",
+    "my backpack",
+    "my money",
+    "my ticket",
+    "my exam"
+  ];
+
+  const when = [
+    "yesterday",
+    "this morning",
+    "when I was coming here",
+    "at some point",
+    "while I was taking a shower"
+  ];
+
+  generateExcuse();
+
+  btn.addEventListener("click", generateExcuse);
+
+  function generateExcuse() {
+    excuse.innerText = `${who[Math.floor(Math.random() * who.length)]} ${
+      what[Math.floor(Math.random() * what.length)]
+    } ${whatAvoid[Math.floor(Math.random() * whatAvoid.length)]} ${
+      when[Math.floor(Math.random() * when.length)]
+    }`;
+  }
 };
